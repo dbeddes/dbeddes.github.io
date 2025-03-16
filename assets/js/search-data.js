948c451316c18682fbf59d29a625b609aee795b0
dbeddes <123787313+dbeddes@users.noreply.github.com>
2025-03-16 05:23:00 +0000

    Deploying to gh-pages from @ dbeddes/dbeddes.github.io@682faee663222c9da31e7ba3c44afd5504d88300 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -346,12 +346,7 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{
+    },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
